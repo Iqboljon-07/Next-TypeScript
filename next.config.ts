b01de823a394576551bf69abe1de3e6d2212092a
Iqboljon Orifjonov <orifjonoviqboljon@gmail.com>
2025-02-21 21:17:26 +0500

    next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  // boshqa sozlamalar
+} as any;
 
 export default nextConfig;
